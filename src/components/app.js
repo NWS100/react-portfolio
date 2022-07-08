5eb8fa899fc36208456c718706169d068603dbf5
Nathan Whitaker <nathan.w424@nebo.local>
2022-07-08 11:53:18 -0600

    more Code

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import moment from "moment"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import PortfolioContainer from './portfolio/portfolio-container'
+import NavigationContainer from './navigation/navigation-container'
+import Home from './pages/home'
+import About from './pages/about'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about-me" component={About} />
+          </Switch>
+        </Router>
+
         <h1>Nathan Whitaker's Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a') }</div>
+        <PortfolioContainer />
       </div>
     );
   }

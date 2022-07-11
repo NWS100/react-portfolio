494691507604b9ef86c6b7784c8c67dc4237f5f7
Nathan Whitaker <nathan.w424@nebo.local>
2022-07-11 08:20:19 -0600

    title works

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
-import moment from "moment"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
+import React, { Component } from "react";
+import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from './portfolio/portfolio-container'
-import NavigationContainer from './navigation/navigation-container'
-import Home from './pages/home'
-import About from './pages/about'
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <NavigationContainer />
-
+      <div className="app">
         <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about-me" component={About} />
-          </Switch>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
         </Router>
 
-        <h1>Nathan Whitaker's Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a') }</div>
+        <h1>Jordan Hudgens Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
       </div>
     );
